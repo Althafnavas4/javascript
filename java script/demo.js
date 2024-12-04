8736f1afe1e3a4ea7eb6a518c7c0c9e3339eb2de
althafnavas4 <nalthaf13@gmail.com>
2024-12-04 09:03:25 +0530

    new

---
+++ java script/demo.js
@@ -0,0 +1,24 @@
+                            //    var let const
+// var a=10
+// var a=20
+// alert(a)
+// a=20
+
+
+// let a=10
+// a=20
+
+// const a=10
+// a=20
+// console.log(a);
+
+// let a=10.20
+// let a='welcome'
+// let a="123"
+// let a={name:'anu',age:22}
+
+
+// let a=null
+// let a=false 
+// console.log(typeof a);
+// console.a=false
