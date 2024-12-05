6a5e916d5ccd32d17dde8e4491ac32a970ce8004
althafnavas4 <nalthaf13@gmail.com>
2024-12-05 09:06:02 +0530

    new

--- java script/demo.js
+++ java script/demo.js
@@ -22,3 +22,45 @@
 // let a=false 
 // console.log(typeof a);
 // console.a=false
+
+
+// function fun(){
+//     let a=document.getElementById("demo").value
+//     let h3=document.getElementById("h3")
+//     h3.innerHTML=a
+// }
+
+
+function add(){
+    let a=parseInt (document.getElementById("f").value)
+    let b=parseInt (document.getElementById("s").value)
+    console.log(a+b);
+    let h3=document.getElementById("h3")
+    h3.innerHTML=a+b
+    
+}
+function sub(){
+    let a=parseInt (document.getElementById("w").value)
+    let b=parseInt (document.getElementById("e").value)
+    console.log(a-b);
+    let h3=document.getElementById("h4")
+    h3.innerHTML=a-b
+
+    
+    
+}
+function mul(){
+    let a=parseInt (document.getElementById("mul1").value)
+    let b=parseInt (document.getElementById("mul2").value)
+    console.log(a*b);
+    let h3=document.getElementById("h4")
+    h3.innerHTML=a*b
+}
+
+function div(){
+    let a=parseInt (document.getElementById("div1").value)
+    let b=parseInt (document.getElementById("div2").value)
+    console.log(a/b);
+    let h3=document.getElementById("h6")
+    h3.innerHTML=a/b
+}
