77d4dffaedab96053aae0fa70b3a6d63f453c1c3
althafnavas4 <nalthaf13@gmail.com>
2024-12-06 09:02:30 +0530

    new

--- java script/demo.js
+++ java script/demo.js
@@ -31,36 +31,50 @@
 // }
 
 
-function add(){
-    let a=parseInt (document.getElementById("f").value)
-    let b=parseInt (document.getElementById("s").value)
-    console.log(a+b);
-    let h3=document.getElementById("h3")
-    h3.innerHTML=a+b
+// function add(){
+//     let a=parseInt (document.getElementById("f").value)
+//     let b=parseInt (document.getElementById("s").value)
+//     console.log(a+b);
+//     let h3=document.getElementById("h3")
+//     h3.innerHTML=a+b
     
-}
-function sub(){
-    let a=parseInt (document.getElementById("w").value)
-    let b=parseInt (document.getElementById("e").value)
-    console.log(a-b);
-    let h3=document.getElementById("h4")
-    h3.innerHTML=a-b
+// }
+// function sub(){
+//     let a=parseInt (document.getElementById("w").value)
+//     let b=parseInt (document.getElementById("e").value)
+//     console.log(a-b);
+//     let h3=document.getElementById("h4")
+//     h3.innerHTML=a-b
 
     
     
-}
-function mul(){
-    let a=parseInt (document.getElementById("mul1").value)
-    let b=parseInt (document.getElementById("mul2").value)
-    console.log(a*b);
-    let h3=document.getElementById("h4")
-    h3.innerHTML=a*b
-}
+// }
+// function mul(){
+//     let a=parseInt (document.getElementById("mul1").value)
+//     let b=parseInt (document.getElementById("mul2").value)
+//     console.log(a*b);
+//     let h3=document.getElementById("h4")
+//     h3.innerHTML=a*b
+// }
 
-function div(){
-    let a=parseInt (document.getElementById("div1").value)
-    let b=parseInt (document.getElementById("div2").value)
-    console.log(a/b);
-    let h3=document.getElementById("h6")
-    h3.innerHTML=a/b
+// function div(){
+//     let a=parseInt (document.getElementById("div1").value)
+//     let b=parseInt (document.getElementById("div2").value)
+//     console.log(a/b);
+//     let h3=document.getElementById("h6")
+//     h3.innerHTML=a/b
+// }
+
+
+
+let a=10
+let b=10
+if (a==b){
+    console.log('eql')
+}
+else if (a!=b){
+    console.log('not')
+}
+else{
+    console.log('else')
 }
---
+++ java script2/demo.js
@@ -0,0 +1,14 @@
+
+// function bonus(){
+//     let a=parseInt (document.getElementById("a").value)
+//     let b=parseInt (document.getElementById("b").value)
+//     let res=document.getElementById("h3")
+//     if (b>=5){
+//         s=a*5/100
+//         ts=a+s
+//         res.innerHTML=ts
+//     }
+//     else{
+//         res.innerHTML='nochange'
+//     }
+// }
